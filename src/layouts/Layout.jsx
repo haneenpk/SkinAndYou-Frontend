476026047af400b811580cbc21cc602b051f91b8
Muhammed Haneen <mohdhaneenpk666@gmail.com>
2024-10-09 09:48:33 +0530

    Fix the redirecting issue of doctor login

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-// import Axios from "../api/shared/instance";
 import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import MainRouter from "../routes/MainRouter";
@@ -13,9 +12,8 @@ import { userRoutesToCheck, doctorRoutesToCheck } from "../config/routesConfig";
 function Layout() {
 
     const dispatch = useDispatch();
-    const isDoctorLoggedIn = useSelector(state => state.doctor.isLoggedIn);
-
     const location = useLocation();
+    const isDoctorLoggedIn = useSelector(state => state.doctor.isLoggedIn);
 
     let userRole = location.pathname.startsWith("/doctor") ? "doctor" : "user";
 
@@ -24,15 +22,18 @@ function Layout() {
         location
     );
 
-    useEffect(async () => {
-        if (localStorage.getItem(`${userRole}JwtToken`)) {
+    // Ensure login state is rehydrated on page load
+    useEffect(() => {
+        const jwtToken = localStorage.getItem(`${userRole}JwtToken`);
+        const doctorData = localStorage.getItem("doctorData");
+
+        if (jwtToken) {
             if (userRole === "doctor") {
                 dispatch(setLoggedIn(true));
-                console.log("changedA:", isDoctorLoggedIn);
-                dispatch(setDoctorData(localStorage.getItem("doctorData")));
+                dispatch(setDoctorData(doctorData));
             }
         }
-    }, [isDoctorLoggedIn])
+    }, [dispatch, userRole]);  // useEffect should run once based on userRole
 
     return (
         <div className="flex flex-col min-h-screen">
@@ -47,4 +48,4 @@ function Layout() {
     )
 }
 
-export default Layout
+export default Layout;
--- src/routes/DoctorRoutes.jsx
+++ src/routes/DoctorRoutes.jsx
@@ -1,51 +1,25 @@
 import { lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 const DoctorHome = lazy(() => import("../pages/doctor/Home"));
-// const Theaters = lazy(() => import("../pages/Admin/Theaters"));
-// const TheaterScreen = lazy(() => import("../pages/Theater/ListScreen"));
-// const EditTheaterScreen = lazy(() => import("../pages/Theater/ViewScreen"));
-// const AddUpcomingMovie = lazy(() => import("../pages/Admin/AddUpcomingMovie"));
-// const EditUpcomingMovie = lazy(() => import("../pages/Admin/EditUpcomingMovie"));
-// const UpcomingMovies = lazy(() => import("../pages/Admin/UpcomingMovies"));
-// const Profile = lazy(() => import("../pages/Admin/Profile"));
-// const EditProfile = lazy(() => import("../pages/Admin/EditProfile"));
-// const Movies = lazy(() => import("../pages/Admin/Movies"));
-// const AddMovies = lazy(() => import("../pages/Admin/AddMovie"));
-// const EditMovies = lazy(() => import("../pages/Admin/EditMovie"));
 import Login from "../pages/doctor/Auth/Login";
 import ErrorPage from "../pages/ErrorPage";
 
 const DoctorRoutes = ({ isLoggedIn }) => {
 
-    const navigateToLogin = () => <Navigate to="/doctor/login" />;
-    const navigateDoctorHome = () => <Navigate to="/doctor" />;
-
-    const routes = [
-        { path: "/", element: isLoggedIn ? <DoctorHome /> : navigateToLogin() },
-        // { path: "/users", element: isLoggedIn ? <Users /> : navigateToLogin() },
-        // { path: "/theaters", element: isLoggedIn ? <Theaters /> : navigateToLogin() },
-        // { path: "/theater-screens", element: isLoggedIn ? <TheaterScreen /> : navigateToLogin() },
-        // { path: "/theater-screen/edit", element: isLoggedIn ? <EditTheaterScreen /> : navigateToLogin() },
-        // { path: "/upcoming/add", element: isLoggedIn ? <AddUpcomingMovie /> : navigateToLogin() },
-        // { path: "/upcoming", element: isLoggedIn ? <UpcomingMovies /> : navigateToLogin() },
-        // { path: "/upcoming/edit", element: isLoggedIn ? <EditUpcomingMovie /> : navigateToLogin() },
-        // { path: "/profile", element: isLoggedIn ? <Profile /> : navigateToLogin() },
-        // { path: "/edit-profile", element: isLoggedIn ? <EditProfile /> : navigateToLogin() },
-        // { path: "/movie", element: isLoggedIn ? <Movies /> : navigateToLogin() },
-        // { path: "/movie/add", element: isLoggedIn ? <AddMovies /> : navigateToLogin() },
-        // { path: "/movie/edit", element: isLoggedIn ? <EditMovies /> : navigateToLogin() },
-
-        // Auth Route
-        { path: "/login", element: !isLoggedIn ? <Login /> : navigateDoctorHome() },
-        // Error Page
-        { path: "/*", element: <ErrorPage /> },
-    ];
-
     return (
         <Routes>
-            {routes.map((route, index) => (
-                <Route key={index} path={route.path} element={route.element} />
-            ))}
+            <Route
+                path="/"
+                element={isLoggedIn ? <DoctorHome /> : <Navigate to="/doctor/login" />}
+            />
+            <Route
+                path="/login"
+                element={!isLoggedIn ? <Login /> : <Navigate to="/doctor" />}
+            />
+            <Route
+                path="/*"
+                element={<ErrorPage />}
+            />
         </Routes>
     );
 };
