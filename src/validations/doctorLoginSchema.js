7b719f014be1153ecc28c8c676cf8e603b8569da
Muhammed Haneen <mohdhaneenpk666@gmail.com>
2024-10-09 10:54:02 +0530

    validate doctor login

--- src/validations/doctorLoginSchema.js
+++ src/validations/doctorLoginSchema.js
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
 export const doctorLoginSchema = Yup.object({
-    email: Yup.string().required("Username is required"),
+    email: Yup.string().email("Invalid email address").required("Email is required"),
     password: Yup.string().required("Password is required"),
 });
